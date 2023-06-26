ff4b444221c969cfc722ae9b0aae6cb12536dfea
iceman404 <johnsubba404@gmail.com>
2023-06-26 11:34:20 +0100

    second commit

--- src/main/java/com/example/visionguardiancommander/LoginApplication.java
+++ src/main/java/com/example/visionguardiancommander/LoginApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -17,13 +19,21 @@ public class LoginApplication extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
         GridPane gridPane = fxmlLoader.load();
-        Scene scene = new Scene(gridPane, 300, 200);
 
+        GridPane root = new GridPane();
+
+        Image gif = new Image(getClass().getResourceAsStream("/assets/images/bh.gif"));
+        ImageView imageView = new ImageView(gif);
+        root.getChildren().add(imageView);
+
+        root.getChildren().add(gridPane); // Add gridPane as a child of root
+
+        Scene scene = new Scene(root, 1280, 720);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
