c162b56f13edc1d9e870348e9d454805ebb1d638
John Subba <69120910+iceman404@users.noreply.github.com>
2023-09-15 13:08:20 +0100

    Add files via upload

---
+++ visionguardiancommander.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2023 at 02:06 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `visionguardiancommander`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdatabase`
+--
+
+CREATE TABLE `userdatabase` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `special_code` varchar(20) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `registration_id` int(10) NOT NULL,
+  `age_info` int(11) NOT NULL,
+  `sex_info` varchar(10) NOT NULL,
+  `photo_directory_path` varchar(100) NOT NULL,
+  `active_status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `userdatabase`
+--
+
+INSERT INTO `userdatabase` (`id`, `special_code`, `first_name`, `last_name`, `registration_id`, `age_info`, `sex_info`, `photo_directory_path`, `active_status`) VALUES
+(13, 'U8', 'John', 'Subba', 45, 23, 'Male', './facesRecord/U8/', 1),
+(16, 'U2', 'Sarita', 'Neupane', 2, 32, 'Female', './facesRecord/U2/', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login_details`
+--
+
+CREATE TABLE `user_login_details` (
+  `id` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_login_details`
+--
+
+INSERT INTO `user_login_details` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin'),
+(2, 'sachin', 'sachin'),
+(3, 'john', 'john');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userdatabase`
+--
+ALTER TABLE `userdatabase`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_login_details`
+--
+ALTER TABLE `user_login_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userdatabase`
+--
+ALTER TABLE `userdatabase`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `user_login_details`
+--
+ALTER TABLE `user_login_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
